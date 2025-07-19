cf2b678e6f1915fb13b6e96d61aa1758e91f76e7
ayman-its <mohamed.ayman@egypt-its.com>
2025-07-19 19:12:32 +0300

    adding the basic file structure

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const { Client } = require('pg');
+const express = require("express");
+const { Client } = require("pg");
 
 const app = express();
 const PORT = 3000;
 
-const PG_CONFIG = {
-    user: 'your_username',
-    host: 'localhost',
-    database: 'testdb',
-    password: 'your_password',
-    port: 5432,
-};
+// const PG_CONFIG = {
+//   user: "monkey_yh8b_user",
+//   host: "dpg-d1s13dbe5dus73flrro0-a",
+//   database: "monkey_yh8b",
+//   password: "QlfrzgJ6LZMlDVlr6ZIj2IOsyCoQzXiF",
+//   port: 5432,
+// };
 
-const client = new Client(PG_CONFIG);
+const client = new Client(
+);
 
-client.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL');
-    })
-    .catch((err) => {
-        console.error('PostgreSQL connection error:', err);
-    });
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("PostgreSQL connection error:", err);
+  });
 
-app.get('/', (req, res) => {
-    res.send('App and database connectivity test successful!');
+app.get("/", (req, res) => {
+  res.send("App and database connectivity test successful!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
